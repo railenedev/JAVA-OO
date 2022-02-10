618bb5466d7ef9501f0d79be0441c2289cecd779
railenedev <railene.dev@gmail.com>
2022-02-10 11:19:18 -0300

    trabalhando com atributos privados e encapsulamento

--- src/bytebankComposto/Conta.java
+++ src/bytebankComposto/Conta.java
@@ -12,7 +12,7 @@ public class Conta {
 
     public boolean saca(double valor) {
         if(this.saldo >= valor) {
-            this.saldo = this.saldo - valor;
+            this.saldo -= valor;
             return true;
         } else {
             return false;
