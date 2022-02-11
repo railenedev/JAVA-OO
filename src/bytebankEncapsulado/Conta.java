2b3c4c7e9f2fb0185513b8b3a321238ce90648cb
railenedev <railene.dev@gmail.com>
2022-02-11 13:01:47 -0300

    adicionando rotina de inicialização em arquivos

--- src/bytebankEncapsulado/Conta.java
+++ src/bytebankEncapsulado/Conta.java
@@ -6,6 +6,13 @@ public class Conta {
     private int numero ;
     private Cliente titular;
 
+
+    public Conta(int agencia, int numero) {
+        this.agencia = agencia;
+        this.numero = numero;
+        System.out.println("estou criando uma conta" + this.numero);
+    }
+
     public void deposita(double valor) {
         this.saldo += valor;
     }
@@ -36,13 +43,22 @@ public class Conta {
     }
 
     public void setNumero(int numero) {
+        if(numero <= 0) {
+            System.out.println("não pode valor <= 0");
+            return;
+        }
         this.numero = numero;
     }
+
     public int getAgencia() {
-        return agencia;
+        return this.agencia;
     }
 
     public void setAgencia(int agencia) {
+        if(agencia <= 0) {
+            System.out.println("não pode valor menor igual a 0");
+            return;
+        }
         this.agencia = agencia;
     }
 
