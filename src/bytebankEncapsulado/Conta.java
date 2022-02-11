7f71edd01f033224695373f252643a0e97b45f32
railenedev <railene.dev@gmail.com>
2022-02-11 13:48:23 -0300

    trabalhando com static

--- src/bytebankEncapsulado/Conta.java
+++ src/bytebankEncapsulado/Conta.java
@@ -5,12 +5,14 @@ public class Conta {
     private int agencia;
     private int numero ;
     private Cliente titular;
-
+    private static int total;
 
     public Conta(int agencia, int numero) {
+        Conta.total++;
+        //System.out.println("o total de contas é " + Conta.total);
         this.agencia = agencia;
         this.numero = numero;
-        System.out.println("estou criando uma conta" + this.numero);
+       // System.out.println("estou criando uma conta " + this.numero);
     }
 
     public void deposita(double valor) {
@@ -64,10 +66,12 @@ public class Conta {
 
     public void setTitular(Cliente titular) {
         this.titular = titular;
-
     }
 
     public Cliente getTitular() {
         return titular;
+    }
+    public static int getTotal() {
+        return Conta.total;
     }
 }
--- src/bytebankEncapsulado/TestaGetESet.java
+++ src/bytebankEncapsulado/TestaGetESet.java
@@ -2,9 +2,8 @@ package bytebankEncapsulado;
 
 public class TestaGetESet {
     public static void main(String[] args) {
-        Conta conta = new Conta();
-        //conta.numero = 1317;
-        conta.setNumero(1337);
+        Conta conta = new Conta(1337, 24226);
+
         System.out.println(conta.getNumero());
 
         Cliente paulo = new Cliente();
